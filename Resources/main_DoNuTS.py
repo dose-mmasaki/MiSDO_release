9d7a99d8a566620aa4aa230406aad873e0dce7d0
Masaki MATSUMOTO <61895986+msk822@users.noreply.github.com>
2021-09-15 18:17:54 +0900

    Revert "Revert "fix (add)""
    This reverts commit 96c4ea40fdb8386eb25bdca4ce7519429bf7e2f9.

--- Resources/main_DoNuTS.py
+++ Resources/main_DoNuTS.py
@@ -1,14 +1,38 @@
 # -*- coding: utf-8 -*-
 from cython_modules import DoNuTS
 import time
+import json
+import pandas as pd
 if __name__ == '__main__':
 
     
     
     result = DoNuTS.main()
-    
+    latest_result = []
+    for x in result:
+        try:
+            for y in x:
+                latest_result.append(y)
+        except :
+            latest_result.append(x)
+
     for modality,res in zip(['CT','PT','NM','XA'],result):
         DoNuTS.writeDB_from_list(modality, res)
+        
+        
+    save_name = './Resources/latest'
+    
+    file_name_json = save_name + ".json"
+    file_name_csv = save_name + ".csv"
+    
+
+    # json
+    with open(file_name_json, mode='wt', encoding='utf-8') as file:
+        json.dump(latest_result, file, ensure_ascii=False, indent=1)
+
+    # csv
+    df = pd.read_json(file_name_json)
+    df.to_csv(file_name_csv, encoding='utf-8')
     
     print("This program will end in 10 seconds")
     time.sleep(10)
